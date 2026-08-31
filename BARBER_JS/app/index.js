ea5a406ebe6cb8a71cf4bd0f656bc07d199c1548
Thembelihle <tafaltein@mandelametro.gov.za>
2026-08-31 16:27:00 +0200

    Adding landing page

--- BARBER_JS/app/index.js
+++ BARBER_JS/app/index.js
@@ -1,7 +1,1 @@
 var app = angular.module('barberApp', []); 
-
-app.controller('IndexController', ['$scope', function($scope) {
-    $scope.message = "Welcome to the Clipper Barbershop.";
-    $scope.imageSrc = "./content/clip 3.png";
-    $scope.loginPage = "Login Page";
-}]);
