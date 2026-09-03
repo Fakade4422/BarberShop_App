a4e820553d57091c2948bcdf521038a128a1a6b1
Thembelihle <tafaltein@mandelametro.gov.za>
2026-09-03 15:36:00 +0200

    landing page

--- BARBER_JS/app/index.js
+++ BARBER_JS/app/index.js
@@ -1,1 +1,15 @@
-var app = angular.module('barberApp', []); 
+'use strict';
+// 1. Declare the module (inject 'ngRoute' if you're using routing)
+var app = angular.module('barberApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'app/components/landing-page/landing-page.html',
+            controller: 'LandingPageController',
+            controllerAs: 'vm'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
