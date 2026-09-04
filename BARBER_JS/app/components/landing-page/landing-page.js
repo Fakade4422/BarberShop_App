959e1665e6a6ce3d0c8cf5ee252ed5c67c9b3495
Thembelihle <tafaltein@mandelametro.gov.za>
2026-09-04 11:06:36 +0200

    starting over

--- BARBER_JS/app/components/landing-page/landing-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-// 1. Declare the module (inject 'ngRoute' if you're using routing)
-var app = angular.module('barberApp', ['ngRoute']);
-
-// 2. Define the controller and attach 'title' to $scope
-app.controller('LandingPageController', ['$scope', function($scope) {
-    $scope.title = "Clippers";
-    $scope.img = "./content/clip 3.png";
-}]);
